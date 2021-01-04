d07d0d7f7eac85d35830536d8ab3cd5c39029171
Rajanayaki S <45528670+Rajanayaki@users.noreply.github.com>
2021-01-04 17:07:17 +0530

    Update Validsubsequence using for.py

--- Validsubsequence using for.py
+++ Validsubsequence using for.py
@@ -1,3 +1,4 @@
+#using for
 def validsubseq(arr,seq):
     subind=0
     for i in arr:
@@ -9,3 +10,17 @@ def validsubseq(arr,seq):
 arr=list(map(int,input().split()))
 seq=list(map(int,input().split()))
 print(validsubseq(arr,seq))
+
+#using while
+def validsubsequence(arr,seq):
+    arrind=0
+    subind=0
+    while arrind<len(arr) and subind<len(seq):
+        if arr[arrind]==seq[subind]:
+            subind+=1
+        arrind+=1
+    return subind==len(seq)
+
+arr=list(map(int,input().split()))
+seq=list(map(int,input().split()))
+print(validsubsequence(arr,seq))
