139a13f6758011285edbb30649f9eaad2e0a1eeb
Rajanayaki S <45528670+Rajanayaki@users.noreply.github.com>
2021-01-04 17:01:36 +0530

    Update twonumber sum using hashtable.py

--- twonumber sum using hashtable.py
+++ twonumber sum using hashtable.py
@@ -1,3 +1,4 @@
+#using hashtable
 #O(N) TC
 #O(N) SC
 def twonumbersum(arr,targetno):
@@ -11,6 +12,29 @@ def twonumbersum(arr,targetno):
             hashtable[i]=True
 
 
+arr=list(map(int,input().split()))
+targetno=int(input())
+print(twonumbersum(arr,targetno))
+
+#using pointers
+#O(N log(N)) TC
+#O(1) SC
+def twonumbersum(arr,targetno):
+    arr.sort()
+    left=0
+    right=len(arr)-1
+    match=False
+    while left<right:
+        currentsum=arr[left]+arr[right]
+        if currentsum==targetno:
+            match=True
+            return (arr[left],arr[right])
+        elif currentsum<targetno:
+            left+=1
+        elif currentsum>targetno:
+            right-=1
+    return match
+        
 arr=list(map(int,input().split()))
 targetno=int(input())
 print(twonumbersum(arr,targetno))
