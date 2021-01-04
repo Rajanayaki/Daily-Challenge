5d1d5e912cd630e577eacc2ee4e29639f14e439b
Rajanayaki S <45528670+Rajanayaki@users.noreply.github.com>
2021-01-04 17:09:46 +0530

    Delete ispalindrome3.py

--- ispalindrome3.py
+++
@@ -1,8 +0,0 @@
-#O(N) TC
-#O(N) SC
-def ispalindrome(string,i=0):
-    j=len(string)-i-1
-    return True if i>=j else string[i]==string[j] and ispalindrome(string,i+1)
-
-string=input()
-print(ispalindrome(string))
